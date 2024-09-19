937c72de8c50272414d467bad5a04a050878fa4d
vishwaskaranth <109661229+vishwaskaranth@users.noreply.github.com>
2024-09-19 22:24:43 +0530

    mem_tb.v

--- Week1/mem_tb.v
+++ Week1/mem_tb.v
@@ -51,36 +51,36 @@ module mem_tb();
         ad = 0;
         Datain = 0;
         
-        $display("Applying reset...");
+       
         rst = 1;
         #10 rst = 0;  
 
-        $display("Test 1: Writing 8'hAA to address 0");
+        
         ad = 4'b0000;   
         Datain = 8'hAA;  
         wr = 1;           
         rd = 0;           
         #10 wr = 0;       
 
-        $display("Reading from address 0");
+        
         rd = 1;           
-        #10 $display("Data at address 0: %h", Dataout); 
+        #10 
         rd = 0;           
 
         
-        $display("Test 2: Writing 8'hBB to address 1");
+        
         ad = 4'b0001;   
         Datain = 8'hBB;  
         wr = 1;          
         rd = 0;           
         #10 wr = 0;       
 
-        $display("Reading from address 1");
+        
         rd = 1;           
-        #10 $display("Data at address 1: %h", Dataout); 
+        #10  
         rd = 0;           
 
-        $display("Test 3: Writing 8'hCC to address 2 (write priority test)");
+       
         ad = 4'b0010;   
         Datain = 8'hCC;  
         wr = 1;           
@@ -88,12 +88,12 @@ module mem_tb();
         #10 wr = 0;       
         
         rd = 0;          
-        $display("Reading from address 2");
+       
         rd = 1;           
-        #10 $display("Data at address 2: %h", Dataout);
+        #10 
         rd = 0;          
 
-        $display("Test completed.");
+        
         $stop;
     end
 
